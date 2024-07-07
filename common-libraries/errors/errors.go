8d583d255de8dc4d843b661d0efe913f1b85dba8
devdone <dkhvan.developer@gmail.com>
2024-07-07 11:55:51 +0500

    feat: Add pointers in common errors

--- common-libraries/errors/errors.go
+++ common-libraries/errors/errors.go
@@ -14,27 +14,27 @@ func (e Error) Error() string {
 	return e.Msg
 }
 
-func NewNotFound(message string) Error {
-	return Error{message, 404}
+func NewNotFound(message string) *Error {
+	return &Error{message, 404}
 }
 
-func NewBadRequest(message string) Error {
-	return Error{message, 400}
+func NewBadRequest(message string) *Error {
+	return &Error{message, 400}
 }
 
-func NewUnauthorized(message string) Error {
-	return Error{message, 401}
+func NewUnauthorized(message string) *Error {
+	return &Error{message, 401}
 }
 
-func NewForbidden(message string) Error {
-	return Error{message, 403}
+func NewForbidden(message string) *Error {
+	return &Error{message, 403}
 }
 
-func NewInternal(message string) Error {
-	return Error{message, 500}
+func NewInternal(message string) *Error {
+	return &Error{message, 500}
 }
 
-func HandleError(w http.ResponseWriter, err Error) {
+func HandleError(w http.ResponseWriter, err *Error) {
 	errJSON := struct {
 		Error string `json:"error"`
 	}{
